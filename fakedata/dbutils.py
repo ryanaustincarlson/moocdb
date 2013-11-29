9e4fed9d2468e545925b7bc934e55b653b9e3372
Ryan Carlson <ryan.austin.carlson@gmail.com>
2013-11-29 18:43:21 -0500

    updated db utils to work on conversation.lti, fixed permissions with a hacky script (setup_db_permissions.sh)

--- fakedata/dbutils.py
+++ fakedata/dbutils.py
@@ -3,9 +3,17 @@ import sys
 
 SOURCE_COURSERA = 1
 
-def get_database_connection(host='localhost', user='rcarlson', password='', database='moocdb'):
+#def get_database_connection(host='localhost', user='rcarlson', password='', database='moocdb'):
+def get_database_connection(unix_sock='/tmp/.s.PGSQL.5432', user='rcarlson', password='', database='moocdb'):
+    '''
+        NOTE: right now (2013-11-29) localhost doesn't resolve properly so we
+              need to point to the socket file address. hopefully in the future we
+              can get the host working so that we don't have to specify this ugly
+              file handle.
+    '''
     db = DBAPI.connect(
-            host=host,
+            unix_sock=unix_sock,
+            #host=host,
             user=user,
             password=password,
             database=database)
@@ -20,7 +28,7 @@ def insert_into_table(cursor, table, columns, values):
 
     values_string = "(" + ', '.join([format_text(v) if type(v) in [str, unicode] else str(v) for v in values]) + ")"
 
-    insert_cmd = "INSERT INTO {} {} VALUES {} RETURNING id;".format(table, columns_string, values_string)
+    insert_cmd = "INSERT INTO {0} {1} VALUES {2} RETURNING id;".format(table, columns_string, values_string)
     sys.stderr.write(insert_cmd + '\n')
 
     cursor.execute(insert_cmd)
